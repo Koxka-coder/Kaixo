31fcfb2f6e759c7e580ee804b77dc6af9b030d42
Joel Martinez <joel.martinez@alumni.mondragon.edu>
2020-07-27 19:25:33 +0200

    Problems with template solved

--- carta/urls.py
+++ carta/urls.py
@@ -1,7 +1,6 @@
-
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.carta, name='carta_list'),
+    path('', views.carta_list, name='carta_list'),
 ]
--- carta/views.py
+++ carta/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-def carta(request):
-    return render(request, 'carta/carta_list.html', {})
+def carta_list(request):
+    return render(request, 'carta/carta.html', {})
