09cd2f656a484fc5c61b95f7a87eb923ac991193
Joel Martinez <joel.martinez@alumni.mondragon.edu>
2020-07-27 20:29:05 +0200

    Primer deploy en python anywhere

--- carta/views.py
+++ carta/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def carta_list(request):
-    return render(request, 'carta/carta.html', {})
+    return render(request, 'carta/index.html', {})
