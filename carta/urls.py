5d340cc5d52fe1d7a07855bcd2d05d701bdb7897
Joel Martinez <joel.martinez@alumni.mondragon.edu>
2020-07-27 21:20:23 +0200

    added all the urls

--- carta/urls.py
+++ carta/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.carta_list, name='carta_list'),
+    path('index', views.carta_list, name='carta_list'),
+    path('about', views.about, name='about'),
+    path('carta', views.carta, name='carta'),
+    path('contact', views.contact, name='contacto'),
 ]
--- carta/views.py
+++ carta/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def carta_list(request):
     return render(request, 'carta/index.html', {})
+    
+def carta(request):
+    return render(request, 'carta/products.html', {})
+
+def about(request):
+    return render(request, 'carta/about.html', {})
+
+def contact(request):
+    return render(request, 'carta/store.html', {})
