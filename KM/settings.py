e6e34edc4dec109726c83361b7aba058bfca3223
Joël Martínez Castro <64122360+Koxka-coder@users.noreply.github.com>
2020-07-27 14:43:09 +0200

    Estructura básica (Con errores)

--- KM/settings.py
+++ KM/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '6^+wxio&z_vegx1m5%kd14q+ysno++9wla7_zd6pjg3-#pzt@5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-es'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Berlin'
 
 USE_I18N = True
 
@@ -118,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
---
+++ carta/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def carta(request):
+    return render(request, 'carta/carta_list.html', {})
